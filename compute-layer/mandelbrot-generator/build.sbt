0608529f01e6ca778e15ecf92d9048201aaa246a
Roderick Timmerman <roderick.timmerman@gmail.com>
2021-08-12 15:39:45 +0100

    added mongos connectivity to the consumer

--- compute-layer/mandelbrot-generator/build.sbt
+++ compute-layer/mandelbrot-generator/build.sbt
@@ -16,3 +16,9 @@ lazy val root = (project in file("."))
 libraryDependencies += "org.apache.kafka" %% "kafka" % "2.8.0"
 libraryDependencies += "org.typelevel" %% "spire" % "0.17.0"
 libraryDependencies += "org.scalanlp" %% "breeze" % "1.2"
+libraryDependencies += "org.mongodb.scala" %% "mongo-scala-driver" % "4.3.1"
+
+fork := true
+// run / javaOptions += "-Djavax.net.debug=all"
+run / javaOptions += "-Djavax.net.ssl.keyStore=/home/roderick/workspace/mongo-cluster/compute-layer/mandelbrot-generator/bbk.ac.uk.p12"
+run / javaOptions += "-Djavax.net.ssl.keyStorePassword=xiec.gate.r"
--- compute-layer/mandelbrot-generator/src/main/scala/example/Consumer.scala
+++ compute-layer/mandelbrot-generator/src/main/scala/example/Consumer.scala
@@ -4,8 +4,21 @@ import org.apache.kafka.clients.consumer._
 import spire.math._
 import spire.implicits._
 import spire.algebra._
+import org.mongodb.scala._
+import org.bson.BsonValue
+import com.mongodb
+import mongodb.client.result._
+import javax.net.ssl.SSLContext
+import java.security.KeyFactory
+import javax.net.ssl.KeyManager
+import javax.net.ssl.KeyManagerFactory
+import java.security.KeyStore
+import javax.net.ssl.TrustManagerFactory
+import java.security.SecureRandom
 
 object Consumer {
+
+  var mongoDbClient: MongoClient = null
 
   def process(z: Complex[Double], c: Complex[Double], iterations: Int): Int = {
     if (iterations < 1) {
@@ -50,6 +63,9 @@ object Consumer {
     topics.add(props.get("topic").asInstanceOf[String])
 
     consumer.subscribe(topics)
+
+    val run0db = mongoDbClient.getDatabase("mandelbrot").getCollection("run0")
+
     while (true) {
       val work = consumer.poll(1000)
       if (work != null)
@@ -63,14 +79,106 @@ object Consumer {
                 new Complex[Double](m.group(1).toDouble, m.group(2).toDouble)
 
               val res = process(z, z, 10)
+
+              val outcome = mongoDbClient
+                .getDatabase("mandelbrot")
+                .getCollection("run0")
+                .insertOne(
+                  Document("r" -> m.group(1), "i" -> m.group(2), "value" -> res)
+                )
+
+              outcome.subscribe(
+                (e: Throwable) => {
+                  throw e
+                },
+                () => {}
+              )
+
               /*if (m.group(1).toDouble >= 2.0)
                 println(if (res > -1) "." else " ")
               else
                 print(if (res > -1) "." else " ")*/
-              println(res) // todo output this.
+              // println(res)
             }
           }
         })
     }
   }
+
+  def getMongoDbClient(): MongoClient = {
+    // connect to the mongodb instance
+
+    if (mongoDbClient != null) {
+      return mongoDbClient
+    }
+
+    println("attempting to connect")
+
+    val uri =
+      "mongodb://mongos-1-svc:27017/mandelbrot?authenticationDatabase=$external&authMechanism=MONGODB-X509"
+
+    val cred = MongoCredential.createMongoX509Credential(
+      "CN=localhost,OU=ExperimentClients,O=Roderick,O=Outside,L=Southmister,ST=Essex,C=UK"
+    )
+
+    val keyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+    keyStore.load(
+      new java.io.FileInputStream(
+        "/home/roderick/workspace/mongo-cluster/compute-layer/mandelbrot-generator/test.ks"
+      ),
+      "xiec.gate.r".toCharArray()
+    )
+
+    val trustStore = KeyStore.getInstance(KeyStore.getDefaultType())
+    trustStore.load(
+      new java.io.FileInputStream(
+        "/home/roderick/workspace/mongo-cluster/compute-layer/mandelbrot-generator/test.ts"
+      ),
+      "xiec.gate.r".toCharArray()
+    )
+
+    val keyManagerFactory =
+      KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm())
+    keyManagerFactory.init(keyStore, "xiec.gate.r".toCharArray())
+
+    val trustManagerFactory =
+      TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+    trustManagerFactory.init(trustStore)
+
+    val sslContext = SSLContext.getInstance("TLS")
+    sslContext.init(
+      keyManagerFactory.getKeyManagers(),
+      trustManagerFactory.getTrustManagers(),
+      new SecureRandom()
+    )
+
+    val dbSettings = MongoClientSettings
+      .builder()
+      .applyConnectionString(ConnectionString(uri))
+      .applyToSslSettings((builder) => {
+        builder.enabled(true).invalidHostNameAllowed(true).context(sslContext)
+      })
+      .credential(cred)
+      .writeConcern(mongodb.WriteConcern.ACKNOWLEDGED)
+      .build()
+
+    mongoDbClient = MongoClient(dbSettings);
+
+    mongoDbClient
+
+    /*println(db.listDatabaseNames().foreach(println));
+    val dataset = Document("r" -> -2, "i" -> -2, "value" -> 3)
+
+    val outcome =
+      db.getDatabase("mandelbrot").getCollection("run0").insertOne(dataset)
+
+    outcome
+      .collect()
+      .subscribe(
+        (e: Throwable) => { println("error: " + e + ", " + e.getCause()) },
+        () => { println("Complete") }
+      )*/
+  }
+
+  mongoDbClient = getMongoDbClient()
 }

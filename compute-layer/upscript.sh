28724961f38b4151e857bc703cd0e7f469f7a9ca
Roderick Timmerman <roderick.timmerman@gmail.com>
2021-10-19 22:35:49 +0100

    adding upscript to compute-layer

---
+++ compute-layer/upscript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source /root/.bashrc
+
+cd /root/mandelbrot-generator
+if [ ${ROLE} = "consumer" ] || [ ${ROLE} = "producer" ]; then 
+    echo "** launching a ${ROLE} node **"
+    setsid sbt "run ${ROLE} ${TOPIC}" &
+
+    # entry point
+    /root/node_exporter-1.0.1.linux-amd64/node_exporter --web.listen-address=0.0.0.0:9091
+fi
+
+if [ ${ROLE} = "producer-shell" ]; then
+    echo '** Producer Shell Ready **'
+    cd /root
+
+    while [ 1 ]; do
+        sleep 1
+    done
+fi
+

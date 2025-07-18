d350e02c46ffd7d247aa76bf8f42001bec7c1b65
Roderick Timmerman <rtimme01@student.bbk.ac.uk>
2025-07-18 13:56:41 +0000

    adding arguments to pull-data for usability

--- prom-data/pull-data.py
+++ prom-data/pull-data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import urllib.request as req
 import urllib.parse
 import datetime
+import sys
 
 class QueryBuilder:
     def __init__(self, baseUrl):
@@ -35,10 +36,23 @@ class QueryBuilder:
         return self.baseUrl + "?" + urllib.parse.urlencode(self.queryParams)
 
 url = ""
+
+params = {
+        'days': 1,
+        'res': '30s',
+        'steps': 45
+}
+key = ''
+for arg in sys.argv:
+    if ('--' in arg):
+        key = arg[2:]
+    elif (len(key) > 0):
+        params[key] = arg
+
 with QueryBuilder("http://rrt-general-services.dcs.bbk.ac.uk:31697") as qb:
-    since = datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(days=4), "%G-%m-%dT%H:%M:%SZ")
+    since = datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(days=int(params['days'])), "%G-%m-%dT%H:%M:%SZ")
     until = datetime.datetime.strftime(datetime.datetime.now(), "%G-%m-%dT%H:%M:%SZ")
-    url = qb.rangeQuery("sum by (instance) (rate(node_cpu_seconds_total{group='consumers',mode='idle'}[4m]))").since(since).until(until).step(45).build()
+    url = qb.rangeQuery(f"sum by (instance) (rate(dwell_time_total[{params['res']}]))").since(since).until(until).step(params['steps']).build()
     try:
         res = req.urlopen(url)
         jsondata = res.read()
@@ -50,7 +64,7 @@ with QueryBuilder("http://rrt-general-services.dcs.bbk.ac.uk:31697") as qb:
         df1['Timestamp'] = list(map(lambda r: r[0], df['data']['result'][0]['values']))
         for i in range(0, 15):
             df1[df['data']['result'][i]['metric']['instance']] = list(map(lambda r: r[1], df['data']['result'][i]['values']))
-        df1.to_csv("./perf-data-2025-07-17-4day.csv")
+        df1.to_csv(f"./perf-data-{until}-{params['days']}day.csv")
 
     except Exception as err:
         print(err.read())
